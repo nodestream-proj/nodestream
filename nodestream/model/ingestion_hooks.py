778b93feefc038bb450d9b617e33bb7c381aef7e
Zach Probst <Zach_Probst@intuit.com>
2023-05-02 07:09:28 -0700

    Port modeling of Ingest Hooks

---
+++ nodestream/model/ingestion_hooks.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Tuple, str, Dict, Any
+
+
+class IngestionHook(ABC):
+    """An IngestionHook is a custom piece of logic that is bundled as a query.
+
+    IngestionHooks provide a mechanism to enrich the graph model that is derived from
+    data added to and currently existing in the graph. For example, drawing an edge
+    between two nodes where following a complex path.
+    """
+
+    @abstractmethod
+    def as_cypher_query_and_parameters(self) -> Tuple[str, Dict[str, Any]]:
+        """Returns a cypher query string and parameters to execute."""
+
+        raise NotImplementedError
+
+
+@dataclass(slots=True, frozen=True)
+class IngestionHookRunRequest:
+    hook: IngestionHook
+    before_ingest: bool
