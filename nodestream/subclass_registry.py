8b7b22c2277098f91da06f8447160a3364b68db4
Zach Probst <Zach_Probst@intuit.com>
2023-05-03 18:04:06 -0700

    Bring in subclass registry and tests

---
+++ nodestream/subclass_registry.py
@@ -0,0 +1,42 @@
+from functools import wraps
+
+
+class AlreadyInRegistryError(ValueError):
+    def __init__(self, name, *args: object) -> None:
+        super().__init__(f"{name} is already registered", *args)
+
+
+class SubclassRegistry:
+    __slots__ = ("registry", "linked_base")
+
+    def __init__(self) -> None:
+        self.registry = {}
+        self.linked_base = None
+
+    def connect_baseclass(self, base_class):
+        old_init_subclass = base_class.__init_subclass__
+        base_class.__subclass__registry__ = self
+        self.linked_base = base_class
+
+        @classmethod
+        @wraps(old_init_subclass)
+        def init_subclass(cls, name=None, *args, **kwargs):
+            if name in self.registry:
+                raise AlreadyInRegistryError(name)
+
+            self.registry[name] = cls
+            return old_init_subclass(*args, **kwargs)
+
+        base_class.__init_subclass__ = init_subclass
+        return base_class
+
+    # TODO: Implement a Bi-dict here?
+    def name_for(self, cls):
+        for k, v in self.registry.items():
+            if v is cls:
+                return k
+
+        return None
+
+    def get(self, name):
+        return self.registry.get(name)
---
+++ tests/unit/test_subclass_registry.py
@@ -0,0 +1,31 @@
+import pytest
+from hamcrest import equal_to, assert_that, none
+
+from nodestream.subclass_registry import SubclassRegistry, AlreadyInRegistryError
+
+
+TEST_REGISTRY = SubclassRegistry()
+
+
+@TEST_REGISTRY.connect_baseclass
+class TestClass:
+    pass
+
+
+class ChildClass(TestClass, name="child"):
+    pass
+
+
+def test_remembers_subclasses_by_name():
+    assert_that(TEST_REGISTRY.get("child"), equal_to(ChildClass))
+    assert_that(TEST_REGISTRY.name_for(ChildClass), equal_to("child"))
+
+
+def test_raises_errors_when_invalid_named_subclass():
+    assert_that(TEST_REGISTRY.get("not_there"), none())
+
+
+def test_raises_errors_when_name_is_resused():
+    with pytest.raises(AlreadyInRegistryError):
+        class _(TestClass, name="child"):
+            pass
