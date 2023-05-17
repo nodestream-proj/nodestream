992cbe86244900f12201c49969dea9f724b97cd6
Zach Probst <Zach_Probst@intuit.com>
2023-05-17 07:39:15 -0700

    Refactor Run Command with polymorphism over conditionals

--- nodestream/cli/commands/run_command.py
+++ nodestream/cli/commands/run_command.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from cleo.commands.command import Command
 from cleo.helpers import argument, option
 
 from ...project import Project, RunRequest, PipelineProgressReporter
 from ...pipeline import PipelineInitializationArguments
+from ...logging import configure_logging_with_json_defaults
 from .async_command import AsyncCommand
 
 
@@ -40,27 +40,17 @@ class RunCommand(AsyncCommand):
         return self.option("json-logging")
 
     async def handle_async(self):
-        self.configure_logging()
-        await self.run_pipeline()
-
-    def configure_logging(self):
-        if not self.use_json_logging:
-            pass
-
-        # TODO: Configure JSON Logging
+        self.pipeline_progress_indicator = self.get_progress_indicator()
+        self.pipeline_progress_indicator.on_start()
+        self.progress_callback = self.pipeline_progress_indicator.progress_callback
+        await self.get_project().run(self.make_run_request())
+        self.pipeline_progress_indicator.on_finish()
 
     def get_project(self) -> Project:
         path = self.option("project")
         if path is not None:
             path = Path(path)
         return Project.from_file(path)
-
-    async def run_pipeline(self):
-        pipeline_name = self.argument("pipeline")
-        self.progress = self.progress_indicator()
-        self.progress.start(f"Running pipeline: '{pipeline_name}'")
-        await self.get_project().run(self.make_run_request())
-        self.progress.finish(f"Finished running pipeline: '{pipeline_name}'")
 
     def make_run_request(self) -> RunRequest:
         return RunRequest(
@@ -69,19 +59,54 @@ class RunCommand(AsyncCommand):
             progress_reporter=self.create_progress_reporter(),
         )
 
+    def get_progress_indicator(self) -> "ProgressIndicator":
+        if self.use_json_logging:
+            configure_logging_with_json_defaults()
+            return ProgressIndicator(self)
+
+        return SpinnerProgressIndicator(self)
+
     def create_pipeline_init_args(self) -> PipelineInitializationArguments:
         return PipelineInitializationArguments(
             annotations=self.option("annotations"),
         )
 
-    def create_progress_reporter(self) -> PipelineProgressReporter:
+    def create_progress_reporter(
+        self,
+    ) -> PipelineProgressReporter:
         return PipelineProgressReporter(
             reporting_frequency=int(self.option("reporting-frequency")),
             callback=self.progress_callback,
         )
 
+
+class ProgressIndicator:
+    def __init__(self, command: RunCommand) -> None:
+        self.command = command
+
+    def on_start(self):
+        pass
+
+    def progress_callback(self, index, record):
+        pass
+
+    def on_finish(self):
+        pass
+
+    @property
+    def pipeline_name(self) -> str:
+        return self.command.argument("pipeline")
+
+
+class SpinnerProgressIndicator(ProgressIndicator):
+    def on_start(self):
+        self.progress = self.command.progress_indicator()
+        self.progress.start(f"Running pipeline: '{self.pipeline_name}'")
+
     def progress_callback(self, index, _):
-        if not self.use_json_logging:
-            self.progress.set_message(
-                f"Currently processing record at index: <info>{index}</info>"
-            )
+        self.progress.set_message(
+            f"Currently processing record at index: <info>{index}</info>"
+        )
+
+    def on_finish(self):
+        self.progress.finish(f"Finished running pipeline: '{self.pipeline_name}'")
