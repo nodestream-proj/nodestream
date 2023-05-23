b0fe2a9f0123031f110f50860c2def800d8714a6
Zach Probst <Zach_Probst@intuit.com>
2023-05-23 08:04:36 -0700

    Add S3 Extractor

---
+++ tests/unit/extractors/stores/aws/conftest.py
@@ -0,0 +1,12 @@
+import os
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def mock_aws_environment_vars():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
--- tests/unit/extractors/stores/aws/test_credential_utils.py
+++ tests/unit/extractors/stores/aws/test_credential_utils.py
@@ -1,27 +1,44 @@
 import datetime
 
-from botocore.session import Session
+import pytest
 
-from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
+@pytest.fixture
+def client_with_role():
+    from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
+    return AwsClientFactory(assume_role_arn="arn:aws:iam::123456789012:role/test")
+
+
+@pytest.fixture
+def client_without_role():
+    from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
+    return AwsClientFactory()
 
 
 def test_init_extra_args_profile():
+    from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
     extra_args = AwsClientFactory._init_session_args(profile_name="test")
     assert "profile_name" in extra_args
 
 
 def test_init_extra_args_empty_profile():
+    from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
     extra_args = AwsClientFactory._init_session_args(profile_name="")
     assert "profile_name" not in extra_args
 
 
 def test_init_extra_args_no_profile():
+    from nodestream.extractors.stores.aws.credential_utils import AwsClientFactory
+
     extra_args = AwsClientFactory._init_session_args()
     assert "profile_name" not in extra_args
 
 
-def test_assume_role_and_get_credentials(mocker):
-    subject = AwsClientFactory(assume_role_arn="arn:aws:iam::123456789012:role/test")
+def test_assume_role_and_get_credentials(mocker, client_with_role):
     mock_sts_client = mocker.patch(
         "nodestream.extractors.stores.aws.credential_utils.boto3.client"
     )
@@ -33,14 +50,13 @@ def test_assume_role_and_get_credentials(mocker):
             "Expiration": datetime.datetime(2020, 1, 1),
         }
     }
-    credentials = subject.assume_role_and_get_credentials()
+    credentials = client_with_role.assume_role_and_get_credentials()
     assert credentials["access_key"] == "test_access_key"
 
 
-def test_get_boto_session_with_refreshable_credentials(mocker):
+def test_get_boto_session_with_refreshable_credentials(mocker, client_with_role):
     # mock assume_role_and_get_credentials and assert that a session was created with refreshable credentials
-    subject = AwsClientFactory(assume_role_arn="arn:aws:iam::123456789012:role/test")
-    subject.assume_role_and_get_credentials = mocker.MagicMock(
+    client_with_role.assume_role_and_get_credentials = mocker.MagicMock(
         return_value={
             "access_key": "test_access_key",
             "secret_key": "test_secret_key",
@@ -48,37 +64,42 @@ def test_get_boto_session_with_refreshable_credentials(mocker):
             "expiry_time": datetime.datetime.now().isoformat(),
         }
     )
-    session = subject.get_boto_session_with_refreshable_credentials()
+    session = client_with_role.get_boto_session_with_refreshable_credentials()
     assert session._credentials.method == "sts-assume-role"
 
 
-def test_assume_role_if_supplied_and_get_session(mocker):
+def test_assume_role_if_supplied_and_get_session(mocker, client_with_role):
     # create a AwsClientFactory with a role arn and mock get_boto_session_with_refreshable_credentials.
     # assert that a the result of get_boto_session_with_refreshable_credentials is what is returned.
-    subject = AwsClientFactory(assume_role_arn="arn:aws:iam::123456789012:role/test")
-    subject.get_boto_session_with_refreshable_credentials = mocker.MagicMock(
+    client_with_role.get_boto_session_with_refreshable_credentials = mocker.MagicMock(
         return_value="test_session"
     )
-    session = subject.assume_role_if_supplied_and_get_session()
+    session = client_with_role.assume_role_if_supplied_and_get_session()
     assert session == "test_session"
 
 
-def test_assume_role_if_supplied_and_get_session_no_role_arn(mocker):
+def test_assume_role_if_supplied_and_get_session_no_role_arn(
+    mocker, client_without_role
+):
+    from botocore.session import Session
+
     # create a AwsClientFactory without a role arn and mock get_boto_session_with_refreshable_credentials.
     # assert that a the result of get_boto_session_with_refreshable_credentials is what is returned.
-    subject = AwsClientFactory()
-    subject.get_boto_session_with_refreshable_credentials = mocker.MagicMock()
-    session = subject.assume_role_if_supplied_and_get_session()
-    subject.get_boto_session_with_refreshable_credentials.assert_not_called()
+    client_without_role.get_boto_session_with_refreshable_credentials = (
+        mocker.MagicMock()
+    )
+    session = client_without_role.assume_role_if_supplied_and_get_session()
+    client_without_role.get_boto_session_with_refreshable_credentials.assert_not_called()
     assert isinstance(session, Session)
 
 
-def test_make_client(mocker):
+def test_make_client(mocker, client_without_role):
     # mock the clal to assume_role_if_supplied_and_get_session.
     # assert that a client was created with the session returned from assume_role_if_supplied_and_get_session.
-    subject = AwsClientFactory()
+    from botocore.session import Session
+
     session = Session()
-    subject.assume_role_if_supplied_and_get_session = mocker.MagicMock(
-        return_value=Session()
+    client_without_role.assume_role_if_supplied_and_get_session = mocker.MagicMock(
+        return_value=session
     )
-    result = subject.make_client("s3")
+    result = client_without_role.make_client("s3")
