c28fd777537a37cc0b2484b577367fd76d0a3175
Zach Probst <zprobst@resilientvitality.com>
2023-05-28 13:18:57 -0700

    Add extra logic in the pipeline progress reporter

--- nodestream/project/pipeline_progress_reporter.py
+++ nodestream/project/pipeline_progress_reporter.py
@@ -6,18 +6,26 @@ from ..pipeline import Pipeline
 from ..utilities import enumerate_async, get_max_mem_mb
 
 
+def no_op(*_, **__):
+    pass
+
+
 @dataclass
 class PipelineProgressReporter:
     """A `PipelineProgressReporter` is a utility that can be used to report on the progress of a pipeline."""
 
     reporting_frequency: int = 1000
     logger: Logger = field(default_factory=getLogger)
-    callback: Callable[[int, Any], None] = field(default_factory=lambda: None)
+    callback: Callable[[int, Any], None] = field(default=no_op)
+    on_start_callback: Callable[[], None] = field(default=no_op)
+    on_finish_callback: Callable[[], None] = field(default=no_op)
 
     async def execute_with_reporting(self, pipeline: Pipeline):
+        self.on_start_callback()
         async for index, record in enumerate_async(pipeline.run()):
             if index % self.reporting_frequency == 0:
                 self.report(index, record)
+        self.on_finish_callback()
 
     def report(self, index, record):
         self.logger.info(
