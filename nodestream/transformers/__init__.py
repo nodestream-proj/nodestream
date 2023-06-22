7cc9da7032dc973c0b2386905f565e41e2d19c33
Zach Probst <zprobst@resilientvitality.com>
2023-06-21 21:33:14 -0700

    Add expand JSON fields

--- nodestream/transformers/__init__.py
+++ nodestream/transformers/__init__.py
@@ -1,3 +1,7 @@
+from .expand_json_field import ExpandJsonField
 from .value_projection import ValueProjection
 
-__all__ = ("ValueProjection",)
+__all__ = (
+    "ExpandJsonField",
+    "ValueProjection",
+)
---
+++ tests/unit/transformers/test_expand_json_field.py
@@ -0,0 +1,19 @@
+import pytest
+from hamcrest import assert_that, equal_to
+
+from nodestream.transformers import ExpandJsonField
+
+
+SIMPLE_INPUT = {"a": 1, "b": '{"hello": "world"}'}
+SIMPLE_OUTPUT = {"a": 1, "b": {"hello": "world"}}
+
+DEEP_INPUT = {"a": 1, "b": {"c": {"d": '{"hello": "world"}'}}}
+DEEP_OUTPUT = {"a": 1, "b": {"c": {"d": {"hello": "world"}}}}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("input,output,path", [(SIMPLE_INPUT, SIMPLE_OUTPUT, "b")])
+async def test_expand_json_fields(input, output, path):
+    subject = ExpandJsonField.from_file_data(path)
+    result = await subject.transform_record(input)
+    assert_that(result, equal_to(output))
