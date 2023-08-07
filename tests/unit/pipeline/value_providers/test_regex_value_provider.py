029b28dfcca6442c81c71ceee983797509af7b9c
Zach Probst <Zach_Probst@intuit.com>
2023-08-07 14:46:07 -0700

    Fix test ordering

--- tests/unit/pipeline/value_providers/test_regex_value_provider.py
+++ tests/unit/pipeline/value_providers/test_regex_value_provider.py
@@ -15,6 +15,13 @@ def subject_with_named_groups():
     )
 
 
+def test_default_group(blank_context, subject_with_named_groups):
+    subject_with_named_groups.group = 0
+    assert_that(
+        subject_with_named_groups.single_value(blank_context), equal_to("john 42")
+    )
+
+
 def test_single_value_matching_regex(blank_context, subject_with_named_groups):
     assert_that(subject_with_named_groups.single_value(blank_context), equal_to("john"))
 
@@ -33,12 +40,6 @@ def test_many_values_not_matching_regex(blank_context, subject_with_named_groups
     subject_with_named_groups.data = StubbedValueProvider(["john", "jane"])
     result = list(subject_with_named_groups.many_values(blank_context))
     assert_that(result, equal_to([None, None]))
-
-
-def test_default_group(blank_context, subject_with_named_groups):
-    assert_that(
-        subject_with_named_groups.single_value(blank_context), equal_to("john 42")
-    )
 
 
 def test_numeric_group(blank_context, subject_with_named_groups):
