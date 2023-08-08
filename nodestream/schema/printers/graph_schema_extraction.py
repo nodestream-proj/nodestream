77579a2fe237890c7152776ce50674cba9d5b2e7
Andrew Ghafari <94899013+AGhafaryy@users.noreply.github.com>
2023-08-08 13:07:55 -0700

    Create graph_schema_extraction.py

---
+++ nodestream/schema/printers/graph_schema_extraction.py
@@ -0,0 +1,25 @@
+from .graphql_schema_printer import GraphQLSchemaPrinter
+from collections import defaultdict
+
+class Neo4jLLMGraphSchemaExtraction(GraphQLSchemaPrinter):
+    def return_nodes_props(self, schema):
+        return {
+            str(node_shape.object_type): node_shape.property_names()
+            for node_shape in schema.known_node_types()
+        }
+
+    def return_rels_props(self, schema):
+        return {
+            str(rel_shape.object_type): rel_shape.property_names()
+            for rel_shape in schema.known_relationship_types()
+        }
+
+    def return_rels(self, schema):
+        rels = defaultdict(list)
+        for elem in schema.relationships:
+            from_node = elem.from_object_type
+            to_node = elem.to_object_type
+            rel_name = elem.relationship_type
+            rels[str(from_node)] = defaultdict(list)
+            rels[str(from_node)][str(rel_name)].append(str(to_node))
+        return rels
