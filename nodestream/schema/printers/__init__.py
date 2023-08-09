1bfc71d69a0fec5b6d2d499c58065a0e3ffc19af
Andrew Ghafari <94899013+AGhafaryy@users.noreply.github.com>
2023-08-09 13:24:45 -0700

    Update __init__.py

--- nodestream/schema/printers/__init__.py
+++ nodestream/schema/printers/__init__.py
@@ -1,12 +1,12 @@
 from .graphql_schema_printer import GraphQLSchemaPrinter
-from .graph_schema_extraction import Neo4jLLMGraphSchemaExtraction
+from .graph_schema_extraction import LargeLanguageModelSchemaPrinter
 from .plain_text_schema_printer import PlainTestSchemaPrinter
 from .schema_printer import SCHEMA_PRINTER_SUBCLASS_REGISTRY, SchemaPrinter
 
 __all__ = (
     "SchemaPrinter",
     "GraphQLSchemaPrinter",
-    "Neo4jLLMGraphSchemaExtraction",
+    "LargeLanguageModelSchemaPrinter",
     "PlainTestSchemaPrinter",
     "SCHEMA_PRINTER_SUBCLASS_REGISTRY",
 )
