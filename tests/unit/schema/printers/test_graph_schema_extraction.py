7d292fd67a684e6f930a0f154e9cff1104eb701f
Andrew Ghafari <94899013+AGhafaryy@users.noreply.github.com>
2023-08-09 12:16:00 -0700

    Update test_graph_schema_extraction.py

--- tests/unit/schema/printers/test_graph_schema_extraction.py
+++ tests/unit/schema/printers/test_graph_schema_extraction.py
@@ -6,6 +6,12 @@ from nodestream.schema.printers.graph_schema_extraction import Neo4jLLMGraphSche
 EXPECTED_NODE_PROPS = {'Person': ['name', 'age'], 'Organization': ['name', 'industry']}
 EXPECTED_RELS = defaultdict(list, {'Person': defaultdict(list, {'BEST_FRIEND_OF': ['Person']}), 'Organization': defaultdict(list, {'HAS_EMPLOYEE': ['Person']})})
 EXPECTED_RELS_PROPS = {'BEST_FRIEND_OF': ['since'], 'HAS_EMPLOYEE': ['since']}
+EXPECTED_PRINTED_SCHEMA = "{'Person': ['name', 'age'], 'Organization': ['name', 'industry']}. {'BEST_FRIEND_OF': ['since'], 'HAS_EMPLOYEE': ['since']}. defaultdict(<class 'list'>, {'Person': defaultdict(<class 'list'>, {'BEST_FRIEND_OF': ['Person']}), 'Organization': defaultdict(<class 'list'>, {'HAS_EMPLOYEE': ['Person']})})"
+
+def test_outputs_schema_correctly(basic_schema):
+    printer = Neo4jLLMGraphSchemaExtraction()
+    output = printer.print_schema_to_string(basic_schema)
+    assert_that(output, equal_to(str(EXPECTED_PRINTED_SCHEMA)))
 
 def test_ensure_nodes_props(basic_schema):
     subject = Neo4jLLMGraphSchemaExtraction()
