34f206491957f08b6ac88373d4042578abac7350
Andrew Ghafari <94899013+AGhafaryy@users.noreply.github.com>
2023-08-10 15:38:49 -0700

    Update __init__.py

--- nodestream/schema/printers/__init__.py
+++ nodestream/schema/printers/__init__.py
@@ -1,5 +1,5 @@
-from .graphql_schema_printer import GraphQLSchemaPrinter
 from .graph_schema_extraction import LargeLanguageModelSchemaPrinter
+from .graphql_schema_printer import GraphQLSchemaPrinter
 from .plain_text_schema_printer import PlainTestSchemaPrinter
 from .schema_printer import SCHEMA_PRINTER_SUBCLASS_REGISTRY, SchemaPrinter
 
