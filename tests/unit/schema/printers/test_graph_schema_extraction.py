94ab0b8e3a7b49dc5a0d96d94f5a1237cf748a81
Andrew Ghafari <94899013+AGhafaryy@users.noreply.github.com>
2023-08-10 15:43:06 -0700

    Update test_graph_schema_extraction.py

--- tests/unit/schema/printers/test_graph_schema_extraction.py
+++ tests/unit/schema/printers/test_graph_schema_extraction.py
@@ -1,27 +1,39 @@
 from collections import defaultdict
+
 from hamcrest import assert_that, equal_to
 
-from nodestream.schema.printers.graph_schema_extraction import LargeLanguageModelSchemaPrinter  
+from nodestream.schema.printers.graph_schema_extraction import (
+    LargeLanguageModelSchemaPrinter,
+)
 
-EXPECTED_NODE_PROPS = {'Person': ['name', 'age'], 'Organization': ['name', 'industry']}
-EXPECTED_RELS = defaultdict(list, {'Person': defaultdict(list, {'BEST_FRIEND_OF': ['Person']}), 'Organization': defaultdict(list, {'HAS_EMPLOYEE': ['Person']})})
-EXPECTED_RELS_PROPS = {'BEST_FRIEND_OF': ['since'], 'HAS_EMPLOYEE': ['since']}
+EXPECTED_NODE_PROPS = {"Person": ["name", "age"], "Organization": ["name", "industry"]}
+EXPECTED_RELS = defaultdict(
+    list,
+    {
+        "Person": defaultdict(list, {"BEST_FRIEND_OF": ["Person"]}),
+        "Organization": defaultdict(list, {"HAS_EMPLOYEE": ["Person"]}),
+    },
+)
 EXPECTED_PRINTED_SCHEMA = "{'Person': ['name', 'age'], 'Organization': ['name', 'industry']}. {'BEST_FRIEND_OF': ['since'], 'HAS_EMPLOYEE': ['since']}. defaultdict(<class 'list'>, {'Person': defaultdict(<class 'list'>, {'BEST_FRIEND_OF': ['Person']}), 'Organization': defaultdict(<class 'list'>, {'HAS_EMPLOYEE': ['Person']})})"
+
 
 def test_outputs_schema_correctly(basic_schema):
     printer = LargeLanguageModelSchemaPrinter()
     output = printer.print_schema_to_string(basic_schema)
     assert_that(output, equal_to(str(EXPECTED_PRINTED_SCHEMA)))
 
+
 def test_ensure_nodes_props(basic_schema):
     subject = LargeLanguageModelSchemaPrinter()
     result = subject.return_nodes_props(basic_schema)
     assert_that(result, equal_to(EXPECTED_NODE_PROPS))
 
+
 def test_ensure_rels(basic_schema):
     subject = LargeLanguageModelSchemaPrinter()
     result = subject.return_rels(basic_schema)
     assert_that(result, equal_to(EXPECTED_RELS))
+
 
 def test_ensure_rels_props(basic_schema):
     subject = LargeLanguageModelSchemaPrinter()
