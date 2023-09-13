02f48ef1feb6813050ae61b3d4eff7213334f109
grantleehoffman <grantleehoffman@gmail.com>
2023-09-13 14:52:25 -0700

    remove unneeded parameter

--- nodestream/project/project.py
+++ nodestream/project/project.py
@@ -223,9 +223,7 @@ class Project(
         """
         for scope in self.scopes_by_name.values():
             for pipeline_definition in scope.pipelines_by_name.values():
-                pipeline_steps = pipeline_definition.initialize_for_introspection(
-                    scope.config
-                ).steps
+                pipeline_steps = pipeline_definition.initialize_for_introspection().steps
                 for idx, step in enumerate(pipeline_steps):
                     if isinstance(step, step_type):
                         yield pipeline_definition, idx, step
