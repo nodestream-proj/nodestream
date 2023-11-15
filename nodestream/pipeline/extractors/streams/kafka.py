41b230ced89c89333744d2885517e37b6ff474c2
grantleehoffman <grantleehoffman@gmail.com>
2023-11-15 15:36:34 -0800

    update to use confluent kafka library

--- nodestream/pipeline/extractors/streams/kafka.py
+++ nodestream/pipeline/extractors/streams/kafka.py
@@ -1,13 +1,28 @@
 from logging import getLogger
 from typing import Any, Iterable, List, Optional
 
-from aiokafka import AIOKafkaConsumer
+from confluent_kafka import Consumer, KafkaError, KafkaException
 from aiokafka.helpers import create_ssl_context
 
 from .extractor import StreamConnector
 
 DEFAULT_GROUP_ID = "nodestream"
 
+class KafkaConsumerFactory:
+    @classmethod
+    def build_consumer_config(cls, offset_reset: str, security_protocol: str, group_id: str, bootstrap_servers: List[str]):
+        return {
+            "bootstrap.servers": ",".join(bootstrap_servers),
+            "auto.offset.reset": offset_reset,
+            "security.protocol": security_protocol,
+            "group.id": group_id,
+        }
+
+    @classmethod
+    def new(cls, topic_name: str, group_id: str, bootstrap_servers: List[str], offset_reset: str, security_protocol: str):
+        consumer = Consumer(cls.build_consumer_config(offset_reset, security_protocol, group_id, bootstrap_servers))
+        consumer.subscribe([topic_name])
+        return consumer
 
 class KafkaStreamConnector(StreamConnector, alias="kafka"):
     """A KafkaStreamConnector implements the StreamConnector interface for Kafka.
@@ -21,54 +36,47 @@ class KafkaStreamConnector(StreamConnector, alias="kafka"):
         bootstrap_servers: List[str],
         topic: str,
         group_id: Optional[str] = None,
+        offset_reset: str = "latest",
         security_protocol: str = "PLAINTEXT",
-        max_poll_interval_ms: int = 30000,
         max_records: int = 10,
-        poll_timeout_ms: int = 30000,
+        poll_timeout: int = 30,
     ):
         self.bootstrap_servers = ",".join(bootstrap_servers)
         self.topic = topic
         self.max_records = 10
         self.group_id = group_id or DEFAULT_GROUP_ID
         self.consumer = None
+        self.offset_reset = offset_reset
         self.security_protocol = security_protocol
-        self.max_poll_interval_ms = (
-            max_poll_interval_ms  # must be set higher than poll timeout
-        )
         self.max_records = max_records
-        self.poll_timeout_ms = poll_timeout_ms
+        self.poll_timeout = poll_timeout
         self.logger = getLogger(__name__)
 
     async def connect(self):
         self.logger.debug("Starting Connection to Kafka Topic %s", self.topic)
-
-        # set default ssl context when using SSL or SASL_SSL security protocol
-        ssl_context = None
-        if self.security_protocol in ["SSL", "SASL_SSL"]:
-            ssl_context = create_ssl_context()
-
-        self.consumer = AIOKafkaConsumer(
-            self.topic,
-            bootstrap_servers=self.bootstrap_servers,
+        self.consumer = KafkaConsumerFactory.new(
+            topic_name=self.topic_name,
             group_id=self.group_id,
-            security_protocol=self.security_protocol,
-            ssl_context=ssl_context,
-            max_poll_interval_ms=self.max_poll_interval_ms,
-            max_poll_records=self.max_records,
+            bootstrap_servers=self.bootstrap_servers,
+            offset_reset=self.offset_reset,
+            security_protocol=self.security_protocol
         )
-
-        await self.consumer.start()
         self.logger.info("Connected to Kafka Topic %s", self.topic)
 
     async def disconnect(self):
-        await self.consumer.stop()
+        await self.consumer.close()
 
     async def poll(self) -> Iterable[Any]:
-        entries = []
-        async for message in self.consumer:
-            self.logger.debug(
-                "Recived Kafka Messages",
-                extra={"topic": message.topic, "partition": message.partition},
-            )
-            entries.append(message.value)
-        return entries
+        results = []
+        for _ in range(self.max_records):
+            msg = self.consumer.poll(timeout=1.0)
+            if msg is None: continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    self.logger.error(f'topic:{msg.topic()} partition:{msg.partition()} reached end at offset {msg.offset()}')
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                results.append(msg)
+        return results
