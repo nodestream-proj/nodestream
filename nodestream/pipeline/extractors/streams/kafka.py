47778cafa6f87b5c93ef9b5126130d84d79c8f27
grantleehoffman <grantleehoffman@gmail.com>
2023-11-16 09:43:43 -0800

    use confluent-kafka lib for kafka extractor

--- nodestream/pipeline/extractors/streams/kafka.py
+++ nodestream/pipeline/extractors/streams/kafka.py
@@ -1,16 +1,23 @@
 from logging import getLogger
 from typing import Any, Iterable, List, Optional
 
-from confluent_kafka import Consumer, KafkaError, KafkaException
 from aiokafka.helpers import create_ssl_context
+from confluent_kafka import Consumer, KafkaError, KafkaException
 
 from .extractor import StreamConnector
 
 DEFAULT_GROUP_ID = "nodestream"
 
+
 class KafkaConsumerFactory:
     @classmethod
-    def build_consumer_config(cls, offset_reset: str, security_protocol: str, group_id: str, bootstrap_servers: List[str]):
+    def build_consumer_config(
+        cls,
+        offset_reset: str,
+        security_protocol: str,
+        group_id: str,
+        bootstrap_servers: List[str],
+    ):
         return {
             "bootstrap.servers": ",".join(bootstrap_servers),
             "auto.offset.reset": offset_reset,
@@ -19,10 +26,22 @@ class KafkaConsumerFactory:
         }
 
     @classmethod
-    def new(cls, topic_name: str, group_id: str, bootstrap_servers: List[str], offset_reset: str, security_protocol: str):
-        consumer = Consumer(cls.build_consumer_config(offset_reset, security_protocol, group_id, bootstrap_servers))
+    def new(
+        cls,
+        topic_name: str,
+        group_id: str,
+        bootstrap_servers: List[str],
+        offset_reset: str,
+        security_protocol: str,
+    ):
+        consumer = Consumer(
+            cls.build_consumer_config(
+                offset_reset, security_protocol, group_id, bootstrap_servers
+            )
+        )
         consumer.subscribe([topic_name])
         return consumer
+
 
 class KafkaStreamConnector(StreamConnector, alias="kafka"):
     """A KafkaStreamConnector implements the StreamConnector interface for Kafka.
@@ -45,7 +64,7 @@ class KafkaStreamConnector(StreamConnector, alias="kafka"):
         self.topic = topic
         self.max_records = 10
         self.group_id = group_id or DEFAULT_GROUP_ID
-        self.consumer = None
+        self.consumer: Consumer = None
         self.offset_reset = offset_reset
         self.security_protocol = security_protocol
         self.max_records = max_records
@@ -55,11 +74,11 @@ class KafkaStreamConnector(StreamConnector, alias="kafka"):
     async def connect(self):
         self.logger.debug("Starting Connection to Kafka Topic %s", self.topic)
         self.consumer = KafkaConsumerFactory.new(
-            topic_name=self.topic_name,
+            topic_name=self.topic,
             group_id=self.group_id,
             bootstrap_servers=self.bootstrap_servers,
             offset_reset=self.offset_reset,
-            security_protocol=self.security_protocol
+            security_protocol=self.security_protocol,
         )
         self.logger.info("Connected to Kafka Topic %s", self.topic)
 
@@ -70,13 +89,12 @@ class KafkaStreamConnector(StreamConnector, alias="kafka"):
         results = []
         for _ in range(self.max_records):
             msg = self.consumer.poll(timeout=1.0)
-            if msg is None: continue
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
-                    # End of partition event
-                    self.logger.error(f'topic:{msg.topic()} partition:{msg.partition()} reached end at offset {msg.offset()}')
-                elif msg.error():
-                    raise KafkaException(msg.error())
-            else:
-                results.append(msg)
+            message_value = self.process_message(msg)
+            results.append(message_value)
         return results
+
+    def process_message(self, msg):
+        if msg.error():
+            raise KafkaException(msg.error())
+        else:
+            return msg.value()
--- tests/unit/pipeline/extractors/streams/test_kafka.py
+++ tests/unit/pipeline/extractors/streams/test_kafka.py
@@ -1,51 +1,69 @@
+from dataclasses import dataclass
+
 import pytest
-from aiokafka import ConsumerRecord
+from confluent_kafka import KafkaException
 from hamcrest import assert_that, equal_to, not_
+from hamcrest.core.core.future import future_raising, resolved
 
 from nodestream.pipeline.extractors.streams import KafkaStreamConnector
 
 
 @pytest.fixture
 def connector():
-    return KafkaStreamConnector(
-        "localhost:9092", "test-topic", max_records=1, poll_timeout_ms=10000
-    )
+    return KafkaStreamConnector("localhost:9092", "test-topic", max_records=1)
 
 
 @pytest.mark.asyncio
 async def test_connect(connector, mocker):
-    mocker.patch("nodestream.pipeline.extractors.streams.kafka.AIOKafkaConsumer.start")
+    mocker.patch("nodestream.pipeline.extractors.streams.kafka.Consumer")
 
     await connector.connect()
     assert_that(connector.consumer, not_(equal_to(None)))
-    connector.consumer.start.assert_called_once()
+    connector.consumer.subscribe.assert_called_once()
 
 
 @pytest.mark.asyncio
 async def test_disconnect(connector, mocker):
     connector.consumer = mocker.AsyncMock()
     await connector.disconnect()
-    connector.consumer.stop.assert_called_once()
+    connector.consumer.close.assert_called_once()
+
+
+@dataclass
+class MockKafkaMessage:
+    msg: str = "test-value"
+    error: str
+
+    def __init__(self, msg, error):
+        self.message = msg
+        self.error_message = error
+
+    def value(self):
+        return self.message
+
+    def error(self):
+        return self.error_message
 
 
 @pytest.mark.asyncio
 async def test_poll(connector, mocker):
-    async def iterator(record):
-        yield record
-
-    test_record = ConsumerRecord(
-        key="",
-        topic="test-topic",
-        value="test-value",
-        partition=None,
-        offset=0,
-        timestamp=0,
-        timestamp_type=1,
-        checksum=1,
-        serialized_key_size=30,
-        serialized_value_size=30,
-        headers=None,
-    )
-    connector.consumer = iterator(test_record)
+    connector.consumer = mocker.Mock()
+    connector.consumer.poll.return_value = MockKafkaMessage("test-value", None)
     result = await connector.poll()
     assert_that(result, equal_to(["test-value"]))
+
+
+@pytest.mark.asyncio
+async def test_poll_error(connector, mocker):
+    connector.consumer = mocker.Mock()
+    connector.consumer.poll.return_value = MockKafkaMessage(None, "error")
+    assert_that(await resolved(connector.poll()), future_raising(KafkaException))
+
+
+@pytest.mark.asyncio
+async def test_poll_infinite_items_terminates(connector, mocker):
+    connector.consumer = mocker.Mock()
+    connector.consumer.poll.return_value = MockKafkaMessage("test-value", None)
+    connector.max_records = 10
+    result = await connector.poll()
+    assert_that(result, equal_to(["test-value"] * 10))
