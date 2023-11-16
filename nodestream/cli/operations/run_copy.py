581937e9e3e859988aa186937bf42782ce532f5e
Zach Probst <Zach_Probst@intuit.com>
2023-11-16 09:14:04 -0800

    Implement and test run copy operation

---
+++ nodestream/cli/operations/run_copy.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from ...databases import Copier, GraphDatabaseWriter
+from ...pipeline import Pipeline
+from ...project import Target
+from ...schema.schema import GraphSchema
+from ..commands.nodestream_command import NodestreamCommand
+from .operation import Operation
+
+
+class RunCopy(Operation):
+    def __init__(
+        self,
+        from_target: Target,
+        to_target: Target,
+        schema: GraphSchema,
+        node_types: List[str],
+        relationship_types: List[str],
+    ) -> None:
+        self.from_target = from_target
+        self.to_target = to_target
+        self.schema = schema
+        self.node_types = node_types
+        self.relationship_types = relationship_types
+
+    async def perform(self, command: NodestreamCommand):
+        pipeline = self.build_pipeline()
+        await pipeline.run()
+
+    def build_pipeline(self) -> Pipeline:
+        copier = self.build_copier()
+        writer = self.build_writer()
+        return Pipeline([copier, writer], step_outbox_size=10000)
+
+    def build_copier(self) -> Copier:
+        return Copier(
+            self.from_target.make_type_retriever(),
+            self.schema,
+            self.node_types,
+            self.relationship_types,
+        )
+
+    def build_writer(self) -> GraphDatabaseWriter:
+        return self.to_target.make_writer()
---
+++ tests/unit/cli/operations/test_run_copy.py
@@ -0,0 +1,51 @@
+import pytest
+from hamcrest import assert_that, equal_to
+
+from nodestream.cli.operations import RunCopy
+
+
+@pytest.fixture
+def subject(mocker):
+    return RunCopy(
+        mocker.Mock(),
+        mocker.Mock(),
+        mocker.Mock(),
+        ["Person", "Movie"],
+        ["ACTED_IN"],
+    )
+
+
+def test_build_writer(subject):
+    result = subject.build_writer()
+    assert_that(result, equal_to(subject.to_target.make_writer.return_value))
+
+
+def test_build_copier(subject):
+    result = subject.build_copier()
+    assert_that(
+        result.type_retriever,
+        equal_to(subject.from_target.make_type_retriever.return_value),
+    )
+    assert_that(result.schema, equal_to(subject.schema))
+    assert_that(result.node_types, equal_to(subject.node_types))
+    assert_that(result.relationship_types, equal_to(subject.relationship_types))
+
+
+def test_build_pipeline(subject, mocker):
+    subject.build_copier = mocker.Mock()
+    subject.build_writer = mocker.Mock()
+    result = subject.build_pipeline()
+    assert_that(
+        result.steps,
+        equal_to(
+            [subject.build_copier.return_value, subject.build_writer.return_value]
+        ),
+    )
+
+
+@pytest.mark.asyncio
+async def test_perform(subject, mocker):
+    pipeline = mocker.AsyncMock()
+    subject.build_pipeline = mocker.Mock(return_value=pipeline)
+    await subject.perform(mocker.Mock())
+    pipeline.run.assert_awaited_once_with()
