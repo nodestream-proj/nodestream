1f20f1b04498fc6895b43a449d34a3918bc068e3
Zach Probst <Zach_Probst@intuit.com>
2023-11-17 11:58:42 -0800

    Modify Target API to support fetching a type retriever

--- nodestream/project/target.py
+++ nodestream/project/target.py
@@ -7,9 +7,18 @@ class Target:
     name: str
     connector_config: Dict[str, Any]
 
+    @property
+    def connector(self):
+        from ..databases import DatabaseConnector
+
+        return DatabaseConnector.from_database_args(**self.connector_config)
+
     def make_writer(self, **writer_args):
         from ..databases import GraphDatabaseWriter
 
         return GraphDatabaseWriter.from_file_data(
             **writer_args, **self.connector_config
         )
+
+    def make_type_retriever(self):
+        return self.connector.make_type_retriever()
--- tests/unit/project/test_target.py
+++ tests/unit/project/test_target.py
@@ -6,3 +6,10 @@ def test_target_make_writer(mocker):
     mock_writer = mocker.patch("nodestream.databases.GraphDatabaseWriter")
     target.make_writer()
     mock_writer.from_file_data.assert_called_once_with(a="b")
+
+
+def test_target_make_type_retriever(mocker):
+    target = Target("test", {"a": "b"})
+    mock_retriever = mocker.patch("nodestream.databases.DatabaseConnector")
+    target.make_type_retriever()
+    mock_retriever.from_database_args.assert_called_once_with(a="b")
