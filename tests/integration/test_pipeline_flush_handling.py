6b4ebc9149f128b04384203331826fc291e94d04
Zach Probst <Zach_Probst@intuit.com>
2023-11-17 07:09:57 -0800

    Add an integration test to prove that flushes generally work

---
+++ tests/integration/test_pipeline_flush_handling.py
@@ -0,0 +1,40 @@
+import pytest
+
+from nodestream.databases import GraphDatabaseWriter
+from nodestream.interpreting import Interpreter
+from nodestream.pipeline import IterableExtractor, Flush, Pipeline
+
+
+@pytest.fixture
+def writer(mocker):
+    return GraphDatabaseWriter(3, mocker.AsyncMock())
+
+
+@pytest.fixture
+def interpreter():
+    return Interpreter.from_file_data(interpretations=[])
+
+
+@pytest.fixture
+def test_extractor_with_flushes(mocker):
+    return IterableExtractor(
+        [
+            Flush,
+            1,
+            2,
+            Flush,
+            3,
+            4,
+            5,
+            6,
+            Flush,
+            7,
+        ]
+    )
+
+
+@pytest.mark.asyncio
+async def test_flush_handling(writer, interpreter, test_extractor_with_flushes):
+    pipeline = Pipeline([test_extractor_with_flushes, interpreter, writer], 1000)
+    await pipeline.run()
+    assert writer.ingest_strategy.flush.call_count == 5
