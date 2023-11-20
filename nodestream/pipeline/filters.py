4e695492dfcd96d1db4d4fb2db6446559f8a8482
Grant Hoffman <grantleehoffman@users.noreply.github.com>
2023-11-20 09:06:45 -0800

    Add regex filter (#163)
    * add regex filters

--- nodestream/pipeline/filters.py
+++ nodestream/pipeline/filters.py
@@ -1,3 +1,4 @@
+import re
 from abc import abstractmethod
 from typing import Any, AsyncGenerator, Dict, Iterable, Optional
 
@@ -90,3 +91,61 @@ class ExcludeWhenValuesMatchPossibilities(Filter):
 
     async def filter_record(self, record: Any) -> bool:
         return not await self.inner.filter_record(record)
+
+
+class RegexMatcher:
+    @classmethod
+    def from_file_data(
+        cls,
+        value: StaticValueOrValueProvider,
+        regex: str,
+        include: bool = True,
+        normalization: Optional[Dict[str, Any]] = None,
+    ):
+        return cls(
+            value_provider=ValueProvider.guarantee_value_provider(value),
+            regex=regex,
+            include=include,
+            normalization=(normalization or {}),
+        )
+
+    def __init__(
+        self,
+        value_provider: StaticValueOrValueProvider,
+        regex: StaticValueOrValueProvider,
+        include: bool,
+        normalization: Dict[str, Any],
+    ) -> None:
+        self.value_provider = value_provider
+        self.regex = re.compile(regex)
+        self.include = include
+        self.normalization = normalization
+
+    def should_include(self, context: ProviderContext) -> bool:
+        actual_value = self.value_provider.normalize_single_value(
+            context, **self.normalization
+        )
+        match = self.regex.match(actual_value) is not None
+        return not match if self.include else match
+
+
+class ValueMatchesRegexFilter(Filter):
+    """A filter that includes/excludes a given value based on a matched regex."""
+
+    @classmethod
+    def from_file_data(
+        cls,
+        value: StaticValueOrValueProvider,
+        regex: StaticValueOrValueProvider,
+        include: bool,
+        normalize: Optional[Dict[str, Any]] = None,
+    ):
+        regex_matcher = RegexMatcher.from_file_data(value, regex, include, normalize)
+        return cls(regex_matcher=regex_matcher)
+
+    def __init__(self, regex_matcher: RegexMatcher):
+        self.regex_matcher = regex_matcher
+
+    async def filter_record(self, item: Any):
+        context_from_record = ProviderContext(item, None)
+        return self.regex_matcher.should_include(context_from_record)
