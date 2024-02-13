f2f4003889b7671b13f60565a5e63519b12cc730
Angelo Santos <angelo_santos@intuit.com>
2024-02-12 17:39:05 -0800

    Added option for global ttl's

--- nodestream/pipeline/extractors/ttls.py
+++ nodestream/pipeline/extractors/ttls.py
@@ -7,13 +7,21 @@ from .extractor import Extractor
 
 class TimeToLiveConfigurationExtractor(Extractor):
     def __init__(
-        self, graph_object_type: str, configurations: Iterable[Dict[str, Any]]
+        self,
+        graph_object_type: str,
+        configurations: Iterable[Dict[str, Any]],
+        global_expiry_in_hours: int = None,
     ) -> None:
         self.graph_object_type = GraphObjectType(graph_object_type)
         self.configurations = configurations
+        self.global_expiry_in_hours = global_expiry_in_hours
 
     async def extract_records(self):
         for config in self.configurations:
+            # Only when global_expiry_in_hours is set, will we have an overwrite of all ttl's
+            if self.global_expiry_in_hours:
+                config["expiry_in_hours"] = self.global_expiry_in_hours
             yield TimeToLiveConfiguration(
-                **config, graph_object_type=self.graph_object_type
+                **config,
+                graph_object_type=self.graph_object_type,
             )
