4f6d4255420b8ffb9cf1da8fdd2f52f80eb44a1d
Zach Probst <Zach_Probst@intuit.com>
2024-02-13 19:04:51 -0800

    Upgrade to support multiple databases

---
+++ tests/unit/cli/operations/test_run_project_cookiecutter.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_perform(mocker, project_dir):
+    from nodestream.cli.operations.run_project_cookiecutter import (
+        RunProjectCookiecutter,
+        PROJECT_COOKIECUTTER_URL,
+    )
+
+    mocked_cookiecutter = mocker.patch(
+        "nodestream.cli.operations.run_project_cookiecutter.cookiecutter"
+    )
+    mocker.patch("nodestream.cli.application.get_version", return_value="0.12.0")
+    subject = RunProjectCookiecutter("project_name", str(project_dir), "neo4j")
+    await subject.perform(mocker.Mock())
+    mocked_cookiecutter.assert_called_once_with(
+        PROJECT_COOKIECUTTER_URL,
+        no_input=True,
+        output_dir=str(project_dir),
+        extra_context={
+            "project_name": "project_name",
+            "database": "neo4j",
+            "nodestream_version": "0.12.0",
+            "nodestream_semver_non_patch_version": "0.12",
+        },
+    )
