976d3912d748cf1212bb2756c484c05154fabba1
grantleehoffman <grantleehoffman@gmail.com>
2024-06-18 05:50:22 -0700

    update queue extractor tests

--- tests/unit/pipeline/extractors/queues/test_extractor.py
+++ tests/unit/pipeline/extractors/queues/test_extractor.py
@@ -4,15 +4,15 @@ import pytest
 from hamcrest import assert_that, equal_to
 
 from nodestream.pipeline import Flush
-from nodestream.pipeline.extractors.streams.extractor import (
+from nodestream.pipeline.extractors.queues.extractor import (
     JsonStreamRecordFormat,
-    StreamExtractor,
+    QueueExtractor,
 )
 
 
 @pytest.fixture
 def extractor(mocker):
-    return StreamExtractor(
+    return QueueExtractor(
         record_format=JsonStreamRecordFormat(),
         connector=mocker.AsyncMock(),
     )
@@ -29,8 +29,6 @@ async def test_extractor_polls_until_error(extractor):
     result = [record async for record in extractor.extract_records()]
     assert_that(result, equal_to(expected_results))
     assert_that(extractor.connector.poll.call_count, equal_to(11))
-    extractor.connector.connect.assert_awaited_once()
-    extractor.connector.disconnect.assert_awaited_once()
 
 
 @pytest.mark.asyncio
@@ -43,5 +41,3 @@ async def test_extractor_polls_empty_list(extractor):
     ]
     result = [record async for record in extractor.extract_records()]
     assert_that(result, equal_to(expected_results))
-    extractor.connector.connect.assert_awaited_once()
-    extractor.connector.disconnect.assert_awaited_once()
