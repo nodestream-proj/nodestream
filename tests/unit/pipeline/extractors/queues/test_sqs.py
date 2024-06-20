e43ebf994f2244c52d06827dd65d076e66a38a2a
grantleehoffman <grantleehoffman@gmail.com>
2024-06-20 05:09:05 -0700

    add addional test for from_file_data

--- tests/unit/pipeline/extractors/queues/test_sqs.py
+++ tests/unit/pipeline/extractors/queues/test_sqs.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from moto import mock_sqs
 
@@ -50,6 +52,50 @@ def subject_no_messages(sqs_queue_no_messages, mock_client):
         max_batches=10,
         sqs_client=mock_client,
     )
+
+
+@pytest.mark.asyncio
+@patch("nodestream.pipeline.extractors.queues.sqs.AwsClientFactory")
+async def test_from_file_data_poll(MockAwsClientFactory, sqs_queue, mock_client):
+    mock_aws_client_factory_instance = MockAwsClientFactory.return_value
+    mock_aws_client_factory_instance.make_client.return_value = mock_client
+
+    queue_url = sqs_queue
+    message_system_attribute_names = ["SenderId", "SentTimestamp"]
+    message_attribute_names = ["CustomAttributeName"]
+    max_batch_size = 5
+    delete_after_read = True
+    max_batches = 5
+    aws_client_args = {"region_name": "us-east-1"}
+
+    # Invoke the from_file_data method to get an instance of SQSQueueConnector
+    sqs_connector = SQSQueueConnector.from_file_data(
+        queue_url=queue_url,
+        message_system_attribute_names=message_system_attribute_names,
+        message_attribute_names=message_attribute_names,
+        max_batch_size=max_batch_size,
+        delete_after_read=delete_after_read,
+        max_batches=max_batches,
+        **aws_client_args
+    )
+
+    # Verify that the properties are correctly assigned
+    assert sqs_connector.queue_url == queue_url
+    assert (
+        sqs_connector.message_system_attribute_names == message_system_attribute_names
+    )
+    assert sqs_connector.message_attribute_names == message_attribute_names
+    assert sqs_connector.max_batch_size == max_batch_size
+    assert sqs_connector.delete_after_read == delete_after_read
+    assert sqs_connector.max_batches == max_batches
+
+    MockAwsClientFactory.assert_called_once_with(**aws_client_args)
+    mock_aws_client_factory_instance.make_client.assert_called_once_with("sqs")
+
+    # Verify that the client works as expected for poll.
+    assert sqs_connector.sqs_client is mock_client
+    results = await sqs_connector.poll()
+    assert results == ["test-message-1", "test-message-2"]
 
 
 @pytest.mark.asyncio
