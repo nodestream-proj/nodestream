7639429fc35ac5cf7b7d303ad97798199fb2855a
ykhaburzaniya <yason_khaburzaniya@intuit.com>
2024-06-26 11:22:02 -0700

    feat/better error message for MappingValueProvider

--- nodestream/pipeline/value_providers/mapping_value_provider.py
+++ nodestream/pipeline/value_providers/mapping_value_provider.py
@@ -3,7 +3,7 @@ from typing import Any, Iterable, Type
 from yaml import SafeDumper, SafeLoader
 
 from .context import ProviderContext
-from .value_provider import StaticValueOrValueProvider, ValueProvider
+from .value_provider import StaticValueOrValueProvider, ValueProvider, ValueProviderException
 
 
 class MappingValueProvider(ValueProvider):
@@ -23,20 +23,26 @@ class MappingValueProvider(ValueProvider):
         self.key = ValueProvider.guarantee_value_provider(key)
 
     def single_value(self, context: ProviderContext) -> Any:
-        mapping = context.mappings.get(self.mapping_name)
-        if not mapping:
-            return
+        try:
+            mapping = context.mappings.get(self.mapping_name)
+            if not mapping:
+                return
 
-        key = self.key.single_value(context)
-        if not key:
-            return
+            key = self.key.single_value(context)
+            if not key:
+                return
 
-        value = ValueProvider.guarantee_value_provider(mapping.get(key))
-        return value.single_value(context)
+            value = ValueProvider.guarantee_value_provider(mapping.get(key))
+            return value.single_value(context)
+        except Exception as e:
+            raise ValueProviderException(str(context.document), self) from e
 
     def many_values(self, context: ProviderContext) -> Iterable[Any]:
         value = self.single_value(context)
         return [value] if value else []
+
+    def __str__(self) -> str:
+        return f"MappingValueProvider: { {'mapping_name': self.mapping_name, 'key': str(self.key)} }"
 
 
 SafeDumper.add_representer(
--- tests/unit/pipeline/value_providers/test_mapping_value_provider.py
+++ tests/unit/pipeline/value_providers/test_mapping_value_provider.py
@@ -3,16 +3,23 @@ from hamcrest import assert_that, equal_to, none
 from yaml import safe_dump
 
 from nodestream.pipeline.value_providers import MappingValueProvider
+from nodestream.pipeline.value_providers.value_provider import ValueProviderException
 
-from ...stubs import StubbedValueProvider
+from ...stubs import StubbedValueProvider, ErrorValueProvider
 
-MAPPINGS = {"MAP_A": {"static": "hello", "dynamic": StubbedValueProvider(["world"])}}
+MAPPINGS = {
+    "MAP_A": {
+        "static": "hello",
+        "dynamic": StubbedValueProvider(["world"]),
+        "error": ErrorValueProvider()
+    }
+}
 
 
 @pytest.fixture
-def blank_context_with_mapping(blank_context):
-    blank_context.mappings = MAPPINGS
-    return blank_context
+def blank_context_with_mapping(blank_context_with_document):
+    blank_context_with_document.mappings = MAPPINGS
+    return blank_context_with_document
 
 
 def test_map_miss(blank_context_with_mapping):
@@ -53,3 +60,34 @@ def test_mapping_dump():
         safe_dump(subject),
         equal_to("!mapping\nkey: !static 'static'\nmapping_name: MAP_A\n"),
     )
+
+
+def test_single_value_error(blank_context_with_mapping):
+    mapping_name = "MAP_A"
+    key = "error"
+    some_text_from_document = blank_context_with_mapping.document["team"]["name"]
+    subject = MappingValueProvider(mapping_name, key)
+
+    with pytest.raises(ValueProviderException) as e_info:
+        subject.single_value(blank_context_with_mapping)
+    error_message = str(e_info.value)
+
+    assert mapping_name in error_message
+    assert key in error_message
+    assert some_text_from_document in error_message
+
+
+def test_multiple_values_error(blank_context_with_mapping):
+    mapping_name = "MAP_A"
+    key = "error"
+    some_text_from_document = blank_context_with_mapping.document["team"]["name"]
+    subject = MappingValueProvider(mapping_name, key)
+
+    with pytest.raises(ValueProviderException) as e_info:
+        iterable = subject.many_values(blank_context_with_mapping)
+        next(iterable)
+    error_message = str(e_info.value)
+
+    assert mapping_name in error_message
+    assert key in error_message
+    assert some_text_from_document in error_message
