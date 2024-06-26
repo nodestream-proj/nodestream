c82e1e6af34865ea4d820e873e4ff92bd394b3b4
ykhaburzaniya <yason_khaburzaniya@intuit.com>
2024-06-26 12:05:26 -0700

    feat/better errors for SplitValueProvider

--- nodestream/pipeline/value_providers/split_value_provider.py
+++ nodestream/pipeline/value_providers/split_value_provider.py
@@ -3,7 +3,7 @@ from typing import Any, Iterable, Type
 from yaml import SafeLoader
 
 from .context import ProviderContext
-from .value_provider import ValueProvider
+from .value_provider import ValueProvider, ValueProviderException
 
 
 class SplitValueProvider(ValueProvider):
@@ -15,15 +15,21 @@ class SplitValueProvider(ValueProvider):
         return next(self.many_values(context))
 
     def many_values(self, context: ProviderContext) -> Iterable[Any]:
-        for value in self.data.many_values(context):
-            if not isinstance(value, str):
-                raise TypeError(
-                    f"Expected {self.data} to yield strings, but got {value!r}"
-                )
-            yield from value.split(self.delimiter)
+        try:
+            for value in self.data.many_values(context):
+                if not isinstance(value, str):
+                    raise TypeError(
+                        f"Expected {self.data} to yield strings, but got {value!r}"
+                    )
+                yield from value.split(self.delimiter)
+        except Exception as e:
+            raise ValueProviderException(str(context.document), self) from e
 
     @classmethod
     def install_yaml_tag(cls, loader: Type[SafeLoader]):
         loader.add_constructor(
             "!split", lambda loader, node: cls(**loader.construct_mapping(node))
         )
+
+    def __str__(self):
+        return f"SplitValueProvider: { {'delimiter': self.delimiter, 'data': str(self.data) } }"
--- tests/unit/pipeline/value_providers/test_split_value_provder.py
+++ tests/unit/pipeline/value_providers/test_split_value_provder.py
@@ -1,8 +1,9 @@
 import pytest
 
 from nodestream.pipeline.value_providers import SplitValueProvider
+from nodestream.pipeline.value_providers.value_provider import ValueProviderException
 
-from ...stubs import StubbedValueProvider
+from ...stubs import StubbedValueProvider, ErrorValueProvider
 
 
 def test_split(blank_context):
@@ -29,3 +30,30 @@ def test_split_no_delimiter(blank_context):
 def test_split_empty(blank_context):
     subject = SplitValueProvider(delimiter=",", data=StubbedValueProvider([""]))
     assert list(subject.many_values(blank_context)) == [""]
+
+
+def test_single_value_error(blank_context_with_document):
+    some_text_from_document = blank_context_with_document.document["team"]["name"]
+    subject = SplitValueProvider(delimiter=",", data=ErrorValueProvider())
+
+    with pytest.raises(ValueProviderException) as e_info:
+        subject.single_value(blank_context_with_document)
+    error_message = str(e_info.value)
+
+    assert subject.delimiter in error_message
+    assert str(subject.data) in error_message
+    assert some_text_from_document in error_message
+
+
+def test_multiple_values_error(blank_context_with_document):
+    some_text_from_document = blank_context_with_document.document["team"]["name"]
+    subject = SplitValueProvider(delimiter=",", data=ErrorValueProvider())
+
+    with pytest.raises(ValueProviderException) as e_info:
+        iterable = subject.many_values(blank_context_with_document)
+        list(iterable)
+    error_message = str(e_info.value)
+
+    assert subject.delimiter in error_message
+    assert str(subject.data) in error_message
+    assert some_text_from_document in error_message
