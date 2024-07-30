73e5f48f3f3f977cfd8abade10e29f6d149a50d0
Zach Probst <Zach_Probst@intuit.com>
2024-07-30 08:55:27 -0700

    Add channel timeout test

--- tests/unit/pipeline/test_channel.py
+++ tests/unit/pipeline/test_channel.py
@@ -42,3 +42,28 @@ async def test_channel_consumer_done():
 
     _, result = await asyncio.gather(consumer(), producer())
     assert result is False
+
+
+@pytest.mark.asyncio
+async def test_channel_times_out_waiting_for_space():
+    input, output = channel(10)
+
+    async def producer():
+        results = [await output.put(i) for i in range(12)]
+        return any(r is False for r in results)
+
+    async def consumer():
+        # wait until the producer has filled the queue to capacity
+        # so that we can be sure that the producer is blocked
+        # waiting for space in the queue
+        while input.channel.queue.qsize() < 10:
+            await asyncio.sleep(0.1)
+
+        # Be extra sure that the producer is blocked by waiting
+        # for a bit longer before calling done on the input channel
+        await asyncio.sleep(0.5)
+
+        # Call done so the input channel will give up waiting for space
+        input.done()
+
+    assert await asyncio.gather(consumer(), producer()) == [None, True]
