71f18736697f4e15360580cd3d1e454ab6d6e6ce
Jon Bristow <jonthegm@yahoo.com>
2025-02-24 15:49:47 -0800

    feat: add an uppercase normalizer to complement lowercase

--- nodestream/pipeline/normalizers/__init__.py
+++ nodestream/pipeline/normalizers/__init__.py
@@ -1,4 +1,5 @@
 from .lowercase_strings import LowercaseStrings
+from .uppercase_strings import UppercaseStrings
 from .normalizer import InvalidFlagError, Normalizer
 from .remove_trailing_dots import RemoveTrailingDots
 from .trim_whitespace import TrimWhitespace
@@ -6,6 +7,7 @@ from .trim_whitespace import TrimWhitespace
 __all__ = (
     "Normalizer",
     "LowercaseStrings",
+    "UppercaseStrings",
     "RemoveTrailingDots",
     "InvalidFlagError",
     "TrimWhitespace",
