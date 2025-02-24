7a7f36b031e1390b09aecaf58613fa2112639968
Jon Bristow <jonthegm@yahoo.com>
2025-02-24 15:55:34 -0800

    fix: sorting of __init__

--- nodestream/pipeline/normalizers/__init__.py
+++ nodestream/pipeline/normalizers/__init__.py
@@ -1,14 +1,14 @@
 from .lowercase_strings import LowercaseStrings
-from .uppercase_strings import UppercaseStrings
 from .normalizer import InvalidFlagError, Normalizer
 from .remove_trailing_dots import RemoveTrailingDots
 from .trim_whitespace import TrimWhitespace
+from .uppercase_strings import UppercaseStrings
 
 __all__ = (
-    "Normalizer",
-    "LowercaseStrings",
-    "UppercaseStrings",
-    "RemoveTrailingDots",
     "InvalidFlagError",
+    "LowercaseStrings",
+    "Normalizer",
+    "RemoveTrailingDots",
     "TrimWhitespace",
+    "UppercaseStrings",
 )
