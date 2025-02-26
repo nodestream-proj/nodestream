f3baaaa0f5320a552839d8ad3636d6c4ca914910
Zach Probst <Zach_Probst@intuit.com>
2025-02-26 08:11:22 -0500

    Condition Interpretations

--- nodestream/interpreting/interpretations/__init__.py
+++ nodestream/interpreting/interpretations/__init__.py
@@ -1,11 +1,12 @@
 from .extract_variables_interpretation import ExtractVariablesInterpretation
-from .interpretation import Interpretation
+from .interpretation import Interpretation, ConditionedInterpretation
 from .properties_interpretation import PropertiesInterpretation
 from .relationship_interpretation import RelationshipInterpretation
 from .source_node_interpretation import SourceNodeInterpretation
 from .switch_interpretation import SwitchInterpretation
 
 __all__ = (
+    "ConditionedInterpretation",
     "ExtractVariablesInterpretation",
     "Interpretation",
     "PropertiesInterpretation",
--- tests/unit/interpreting/interpretations/test_interpretation.py
+++ tests/unit/interpreting/interpretations/test_interpretation.py
@@ -3,6 +3,11 @@ from hamcrest import assert_that, instance_of
 from nodestream.interpreting.interpretations import (
     Interpretation,
     SourceNodeInterpretation,
+    ConditionedInterpretation,
+)
+from nodestream.interpreting.interpretations.conditions import (
+    AlwaysTrue,
+    ProviderContext,
 )
 
 
@@ -11,3 +16,33 @@ def test_from_file_data_gets_right_subclass():
         type="source_node", node_type="Test", key={"key": "value"}
     )
     assert_that(result, instance_of(SourceNodeInterpretation))
+
+
+def test_from_file_data_gets_condition_handled():
+    result = Interpretation.from_file_data(
+        type="source_node",
+        node_type="Test",
+        key={"key": "value"},
+        condition={"type": "true"},
+    )
+    assert_that(result, instance_of(ConditionedInterpretation))
+    assert_that(result.condition, instance_of(AlwaysTrue))
+    assert_that(result.interpretation, instance_of(SourceNodeInterpretation))
+
+
+def test_conditioned_interpretation_continues_on_condition_matched(mocker):
+    context = mocker.Mock(spec=ProviderContext)
+    interpretation = mocker.Mock(spec=SourceNodeInterpretation)
+    conditioned = ConditionedInterpretation(AlwaysTrue(), interpretation)
+    conditioned.interpret(context)
+    interpretation.interpret.assert_called_once_with(context)
+
+
+def test_conditioned_interpretation_breaks_on_missing_condition(mocker):
+    context = mocker.Mock(spec=ProviderContext)
+    interpretation = mocker.Mock(spec=SourceNodeInterpretation)
+    definitely_totally_true = mocker.Mock(spec=AlwaysTrue)
+    definitely_totally_true.evaluate.return_value = False
+    conditioned = ConditionedInterpretation(definitely_totally_true, interpretation)
+    conditioned.interpret(context)
+    interpretation.interpret.assert_not_called()
