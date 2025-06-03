b64143805b8a2bbb8e33c924d973ca01ba0170b5
Angelo Santos <angelo_santos@intuit.com>
2025-06-03 16:58:31 -0700

    Removing experimental migration metrics

--- nodestream/cli/commands/run_migrations.py
+++ nodestream/cli/commands/run_migrations.py
@@ -1,9 +1,8 @@
 from cleo.helpers import option
 
-from ..operations import ExecuteMigrations, InitializeMetricsHandler
+from ..operations import ExecuteMigrations
 from .nodestream_command import NodestreamCommand
-from .shared_options import PROJECT_FILE_OPTION, TARGETS_OPTION, PROMETHEUS_OPTIONS
-from ...metrics import Metrics
+from .shared_options import PROJECT_FILE_OPTION, TARGETS_OPTION
 
 
 class RunMigrations(NodestreamCommand):
@@ -13,24 +12,21 @@ class RunMigrations(NodestreamCommand):
         PROJECT_FILE_OPTION,
         TARGETS_OPTION,
         option("all-targets", "a", "Run migrations on all targets", flag=True),
-        *PROMETHEUS_OPTIONS,
     ]
 
     async def handle_async(self):
-        with Metrics.capture():
-            await self.run_operation(InitializeMetricsHandler())
-            project = self.get_project()
-            migrations = self.get_migrations()
+        project = self.get_project()
+        migrations = self.get_migrations()
 
-            if self.option("all-targets"):
-                targets = [target for target in project.targets_by_name]
-            else:
-                targets = self.option(TARGETS_OPTION.name)
+        if self.option("all-targets"):
+            targets = [target for target in project.targets_by_name]
+        else:
+            targets = self.option(TARGETS_OPTION.name)
 
-            if len(targets) == 0:
-                self.info("No targets specified, nothing to do.")
-                return
+        if len(targets) == 0:
+            self.info("No targets specified, nothing to do.")
+            return
 
-            for target_name in targets:
-                target = project.get_target_by_name(target_name)
-                await self.run_operation(ExecuteMigrations(migrations, target))
+        for target_name in targets:
+            target = project.get_target_by_name(target_name)
+            await self.run_operation(ExecuteMigrations(migrations, target))
