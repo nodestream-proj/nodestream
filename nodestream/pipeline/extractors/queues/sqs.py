4becef1332163a10b9d19b91cff364d1cf847a17
Angelo Santos <angelo_santos@intuit.com>
2026-03-25 10:58:38 -0700

    Making the extractor call a connectors class method constructor for intermediate paramter handling.

--- nodestream/pipeline/extractors/queues/sqs.py
+++ nodestream/pipeline/extractors/queues/sqs.py
@@ -2,6 +2,7 @@ import asyncio
 from logging import getLogger
 from typing import Any, AsyncGenerator, Iterable, Optional
 
+
 from nodestream.pipeline.extractors.queues.extractor import QueueConnector
 
 from ..credential_utils import AwsClientFactory
